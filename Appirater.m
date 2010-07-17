6e0a9a2324a19a75fc4e54abe0b55e13c39c86e8
Scotty Allen <scotty@ooyala.com>
2010-07-17 11:39:19 -0700

    Whitespace.

--- Appirater.m
+++ Appirater.m
@@ -62,28 +62,28 @@ NSString *templateReviewURL = @"http://phobos.apple.com/WebObjects/MZStore.woa/w
     bzero(&zeroAddress, sizeof(zeroAddress));
     zeroAddress.sin_len = sizeof(zeroAddress);
     zeroAddress.sin_family = AF_INET;
-	
+
     // Recover reachability flags
     SCNetworkReachabilityRef defaultRouteReachability = SCNetworkReachabilityCreateWithAddress(NULL, (struct sockaddr *)&zeroAddress);
     SCNetworkReachabilityFlags flags;
-	
+
     BOOL didRetrieveFlags = SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags);
     CFRelease(defaultRouteReachability);
-	
+
     if (!didRetrieveFlags)
     {
         NSLog(@"Error. Could not recover network reachability flags");
         return NO;
     }
-	
+
     BOOL isReachable = flags & kSCNetworkFlagsReachable;
     BOOL needsConnection = flags & kSCNetworkFlagsConnectionRequired;
     BOOL nonWiFi = flags & kSCNetworkReachabilityFlagsTransientConnection;
-	
+
     NSURL *testURL = [NSURL URLWithString:@"http://www.apple.com/"];
     NSURLRequest *testRequest = [NSURLRequest requestWithURL:testURL  cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:20.0];
     NSURLConnection *testConnection = [[NSURLConnection alloc] connectionWithRequest:testRequest delegate:self];
-	
+
     return ((isReachable && !needsConnection) || nonWiFi) ? (testConnection ? YES : NO) : NO;
 }
 
@@ -102,8 +102,8 @@ Appirater *appirater;
 }
 
 - (void)dealloc {
-  self.alertView = nil;
-  [super dealloc];
+	self.alertView = nil;
+	[super dealloc];
 }
 
 - (void)_appLaunched {
